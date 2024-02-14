1829d31efda850690928331244e78042d7f8db56
Kalsar <114978816+kalsar@users.noreply.github.com>
2024-02-14 23:39:43 +0530

    Update footer.js

--- src/pcomponent/footer.js
+++ src/pcomponent/footer.js
@@ -5,13 +5,13 @@ const Footer = () => {
         <div className="footer-section">
             <p className="title">FoodHub.com</p>
             <p className='mm'>FoodHub is a place here you can please your soul and tummy with delicious food recipes of all cuisine and our services are absolutely free.</p>
-            <p>&copy;2023 | Akash Agrawal<br/>All Rights Reserved</p>
+            <p>&copy;2023 | Kalsar Ali<br/>All Rights Reserved</p>
         </div>
         <div className="footer-section">
             <p className="title">Contact Us</p>
             <p>info@foodhub.com</p>
-            <p>9149215780</p>
-            <p>Aligarh</p>
+            <p>9870698361</p>
+            <p>Noida</p>
         </div>
         <div className="footer-section">
             <p className="title">Socials</p>
